5d53a868fba2c748973629a3445e8af89740e933
Neha Sudarshan <neha.sudarshan189@gmail.com>
2025-07-24 18:05:04 -0400

    Add files client, portfolio, security - Follows same format as advisor.java - autogenerated id fields portfolioID, clientID, securityID - added accessors and mutators

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,49 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioID;
+
+    @Column(nullable = false)
+    private long clientID;
+
+    @Column(nullable = false)
+    private long advisorID;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(long advisorID, long clientID) {
+        this.advisorID = advisorID;
+        this.clientID = clientID;
+    }
+
+    public Long getPortfolioID() {
+        return this.portfolioID;
+    }
+
+    public Long getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(long clientID) {
+        this.clientID = clientID;
+    }
+
+    public Long getAdvisorID() {
+        return this.advisorID;
+    }
+
+    public void setAdvisorID(long advisorID) {
+        this.advisorID = advisorID;
+    }
+}
